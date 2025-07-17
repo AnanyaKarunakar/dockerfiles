e81e283707b8285c4bb0d1f30385ae7b07215210
karna <karunakark1729@gmail.com>
2025-07-17 20:30:05 +0530

    docker install.sh

---
+++ docker-install.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+sudo su -
+
+dnf -y install dnf-plugins-core
+
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+usermod -aG docker ec2-user
+
+exit
+
+
